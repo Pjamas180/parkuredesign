c9715f20bdb81de0fb7ec04b67aacdec0a223720
Pedro Villaroman <pedrovillaroman@gmail.com>
2016-02-11 23:19:38 -0800

    adding json stuff

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,12 @@
+var usernames = require("../json/accounts.json");
+
 exports.view = function(req, res) {
 	console.log("Viewing index file");
 	res.render('index');
+}
+
+exports.login = function(req, res) {
+	console.log("User is logging in!");
+
+	res.render('home');
 }
