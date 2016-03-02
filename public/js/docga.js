b765365d5dfe9afdd73cc3657f21b8e8a77b66ab
Pedro Villaroman <pedrovillaroman@gmail.com>
2016-03-02 15:29:57 -0800

    Changed docga.js

--- public/js/docga.js
+++ public/js/docga.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
 
+	$('#logo').click(function() {
+		console.log("logo clicked");
+		ga('send', 'event', 'logo_click', 'click');
+	});
+	/*
 	$('#logo').on('click', function(event) {
 		console.log("logo clicked");
 		ga('send', 'event', 'logo_click', 'click');
 	})
+	*/
 
 });
